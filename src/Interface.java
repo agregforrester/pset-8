f815031e8f7cc54c21126690a51a892b975fb0a8
Aidan Forrester <agregforrester@gmail.com>
2020-11-08 20:26:30 -0500

    final

---
+++ src/Interface.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Interface {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Account No.:");
+
+        
+
+    }
+}
